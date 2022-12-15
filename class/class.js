d61267c67aca3391fd90e3b8d252afc7283476f3
owen22s <114576146+owen22s@users.noreply.github.com>
2022-12-15 10:20:25 +0100

    Add files via upload

---
+++ class/class.js
@@ -0,0 +1,24 @@
+class Telefoon{
+    constructor(){
+        this.beeldscherm = 4
+        this.video = 560
+        this.camera = "HD"
+        
+    } 
+    bellen()
+    {
+        
+    }
+    
+    fotomaken()
+    {
+        
+    }
+
+    appen()
+    {
+        
+    } 
+  }
+
+let telefoon = new Telefoon
